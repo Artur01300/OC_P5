a85a2ae26ad575a92a6128cbe15f9be3cbe844f4
Artur01300 <artur.karapetyan@outlook.fr>
2020-10-07 06:40:58 +0200

    Modifcations apportés après le session du 05/10/20 - 100% -

--- frontend/javascript/panier.js
+++ frontend/javascript/panier.js
@@ -23,8 +23,7 @@ function listenForProductDeletion(products){
   for(let product of products){
     document.getElementById('delete-' + product._id).addEventListener('click', function(){
 
-      if(productIds.includes(product._id)){    
-        console.log(productIds)        
+      if(productIds.includes(product._id)){         
         let index = productIds.indexOf(product._id);           
         productIds.splice(index,1);
         if(productIds.length === 0){
@@ -67,76 +66,113 @@ function hide(id){
 
 function getProductsFromCart(products){
   let list = [];
-  let productIds = get('products');//on récupaire le productIds
+  // let productIds = get('products');//je pence qu'on a pas besoin récupéer le get('products') ?
   for(const productId of productIds){
     for(let product of products){
-      if(productId == product._id){        
+      if(productId == product._id){  // si le produit existe on push le poduit on questionn
         list.push(product)
       }
     }      
   }
-  return list; //returner la liste une fois le boucle est términé
+  return list; //returner la liste une fois le boucle est términé. il retourne tous les produits du panier
 }
 
 // ******************************** Validation Email *********************************
 
 function listenForFormSubmit(){
+  let form = document.querySelector('#myForm');
+
   form.addEventListener('submit', function(e){
     e.preventDefault();
-  
-    if(fromInput(form.email) && validZipcode(form.zipcode)){
-      form.submit();
-      window.location.href = "commande.html";
+    checkInputs();
+    // if(!checkInputs()){
+    //   alert('Merci de corriger le formoulaire');
+    //   return;
+    // }
+
+    let payload = {
+      contact: {
+        firstName: document.getElementById('firstname').value,
+        lastName: document.getElementById('lastname').value,
+        address: document.getElementById('adresse').value,
+        city: document.getElementById('city').value,
+        email: document.getElementById('email').value
+      },
+      products: get('products')
     }
+
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      request.onreadystatechange = function() {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+          let response = JSON.parse(this.responseText);
+          resolve(response);
+        }
+      };
+      request.open("POST", "http://localhost:3000/api/furniture/order");
+      // request.send(JSON.stringify(payload));
+      payload = JSON.stringify(payload);
+      request.send(payload);
+    });
   });
 }
 
-let form = document.querySelector('#myForm');
+function checkInputs(){
+  let email = document.getElementById('email').value;
+  let firstname = document.getElementById('firstname').value;
+  let lastname = document.getElementById('lastname').value;
+  let adresse = document.getElementById('adresse').value;
+  let city = document.getElementById('city').value;
 
-form.email.addEventListener('change', function(){
-  fromInput(this);
-});
-
-function fromInput(inputEmail){
-  let emailRegExp = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g');
-  let testEmail = emailRegExp.test(inputEmail.value);
-  let small = inputEmail.nextElementSibling;
-
-  if(testEmail){
-    small.innerHTML = '';
-    return true;
+  if(isEmailVald(email)){
+    messagErreur('')
   }else{
-    small.innerHTML = 'Adresse e-mail non valide. Veuillez ajoutez un adresse e-mail valide.';
+    messagErreur('Ce champ est obligatoire.')
     return false;
   }
-};
 
-// ******************************** Validation Zipcode *********************************
-
-form.zipcode.addEventListener('change', function(){
-  validZipcode(this);
-})
-
-function validZipcode (inputZipCode){
-  let regExpZipCode = /^[0-9]{5}$/;
-  let testZipCode = regExpZipCode.test(inputZipCode.value);
-  let small = inputZipCode.nextElementSibling;
-
-  if(testZipCode){
-    small.innerHTML = '';
-    return true;
+  if(isNameVald(firstname)){
+    messagErreur('');
   }else{
-    small.innerHTML = 'Code postale non valide. Veuillez ajoutez un code postale valide.';
+    messagErreur('Ce champ est obligatoire.');
+    return false;
+  }
+
+  if(isNameVald(lastname)){
+    messagErreur('');
+  }else{
+    messagErreur('Ce champ est obligatoire.');
+    return false;
+  }
+
+  if(isAdresseValide(adresse)){
+    messagErreur('');
+  }else{
+    messagErreur('Ce champ est obligatoire.');
+    return false;
+  }
+
+  if(isAdresseValide(city)){
+    messagErreur('');
+  }else{
+    messagErreur('Ce champ est obligatoire.');
     return false;
   }
 }
 
-// ******************************** Validation Formulaire *********************************
-// form.addEventListener('submit', function(e){
-//   e.preventDefault();
+function messagErreur(messages){
+  document.getElementById('msg').innerHTML = messages;
+}
 
-//   if(fromInput(form.email) && validZipcode(form.zipcode)){
-//     form.submit();
-//     window.location.href = "commande.html";
-//   }
-// });
+function isNameVald(name){
+  return name.length > 3;
+}
+
+function isEmailVald(email){
+  let regExp = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g');
+  return regExp.test(email);
+}
+
+function isAdresseValide(adresse){
+  return adresse.length > 3;
+}
